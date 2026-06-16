51a82abca63cbaa6524cf094c728bb027378bdc8
vsdifficult <newfakes46578@gmail.com>
2026-06-16 13:47:38 +0300

    fix(buildingblocks): field repetition remove in AuditableEntity

--- src/SecondBrain.BuildingBlocks/Core/Entities/AuditableEntity.cs
+++ src/SecondBrain.BuildingBlocks/Core/Entities/AuditableEntity.cs
@@ -1,11 +1,8 @@
-
 namespace SecondBrain.BuildingBlocks.Core.Entities; 
 
-public abstract class AuditableEntity: BaseEntity
+public abstract class AuditableEntity : BaseEntity
 {
-    public DateTime CreatedAt { get; set; }
-    public string CreatedBy { get; set; }
-    public DateTime? UpdatedAt { get; set; }
-    public string UpdatedBy { get; set; }
+    public string? CreatedBy { get; set; }
+    public string? UpdatedBy { get; set; }
     public bool IsDeleted { get; set; } // Soft Delete
 }
